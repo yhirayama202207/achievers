0cf5229fb93d2d3b362abbd0874919ee13e58510
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2023-01-29 00:25:09 +0000

    Add routings

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   devise_for :admins
-  # devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
